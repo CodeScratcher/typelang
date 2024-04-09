38574c2aaaf866b1506d18988ec90a320e336677
Arielle Fisher <bb8andr2d2forlife@gmail.com>
2024-04-08 22:02:09 -0400

    Starting working on hello world

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+open Llvm
+exception Error of string
+
+let context = global_context ()
+let tl_mod = create_module context "TypeLang"
+let builder = builder context
+let printf_type = var_arg_function_type (i32_type context) [|pointer_type context|];;
+
+declare_function "printf" printf_type tl_mod;;
+
+let callee =
+  match lookup_function "printf" tl_mod with
+  | Some callee -> callee
+  | None -> raise (Error "unknown function referenced")
+
+let args = [|build_global_stringptr "Hello World!" "strtmp" builder|];;
+
+
+dump_value (build_call printf_type callee args "calltmp" builder)
