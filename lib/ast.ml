e617c369e5e2ad471b7d339e4da95b68e7663011
Arielle F <bb8andr2d2forlife@gmail.com>
2024-05-01 13:51:39 -0400

    Wrote a parser, testing soon

--- lib/ast.ml
+++ lib/ast.ml
@@ -4,6 +4,13 @@ type atom =
   | IdentAtom of string
   | FloatAtom of float
 
+let printAtom atom = match atom with
+  | IntAtom x -> String.cat "Int: " (string_of_int x)
+  | StringAtom x -> String.cat "String: " x 
+  | IdentAtom x -> String.cat "Ident: " x
+  | FloatAtom x -> String.cat "Float: " (string_of_float x)
+
 type sexpr = 
   | AtomExpr of atom
   | ListExpr of sexpr list
+  | ParseError of string
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,13 +1,36 @@
 open Lexer 
 open Ast
 
-let compose f g x = f (g x)
-
 let int_parse x err = 
   match int_of_string_opt x with
-    | Option.Some x -> Result.Ok (IntAtom x)
-    | Option.None -> Result.Error err
+    | Option.Some x -> AtomExpr (IntAtom x)
+    | Option.None -> ParseError err
 
-let rec parse tokens = match List.hd tokens with
-  | Number x -> int_parse x "Integer parsing error" :: parse (List.tl tokens)
-  | _ -> [Result.Error "Unimplemented"]
+let parseAtom token = match token with 
+  | Number x -> int_parse x "Wrong type"
+  | Ident x -> (AtomExpr (IdentAtom x))
+  | String x -> (AtomExpr (StringAtom x))
+  | _ -> ParseError "Wrong token"
+
+let rec parseList tokens = match tokens with
+  | [] -> ([ParseError "Unexpected end of file, expecting right parenthesis"], [])
+  | head :: tail -> (match head with 
+    | RParen -> ([], tail)
+    | LParen -> let parsedList = parseList tail in 
+      let parsedTail = parseList (snd parsedList) in 
+        (ListExpr (fst parsedList) :: fst parsedTail, snd parsedTail)
+    | Number x -> let parsedTail = parseList tail in (int_parse x "Wrong type" :: fst parsedTail, snd parsedTail)
+    | Ident x -> let parsedTail = parseList tail in (AtomExpr (IdentAtom x) :: fst parsedTail, snd parsedTail)
+    | String x -> let parsedTail = parseList tail in (AtomExpr (StringAtom x) :: fst parsedTail, snd parsedTail)
+    | EOF -> ([ParseError "Unexpected end of file, expecting right parenthesis"], []))
+  
+let rec parse tokens = match tokens with
+  | [] -> []
+  | head :: tail -> (match head with
+    | RParen -> [ParseError "Unopened right parenthesis"]
+    | LParen -> let parsedList = parseList tail in ListExpr (fst parsedList) :: parse (snd parsedList)
+    | Number x -> int_parse x "Wrong type" :: parse tail
+    | Ident x -> AtomExpr (IdentAtom x) :: parse tail
+    | String x -> AtomExpr (StringAtom x) :: parse tail
+    | EOF -> [])
+
